f596dfe2bd268c8b149e62c535a4b791cef6f84b
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-03-06 21:40:42 -0600

    Add Domain Logic for Cart

--- services/cart_service/internal/core/domain/cart.go
+++ services/cart_service/internal/core/domain/cart.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -11,7 +12,7 @@ type Cart struct {
 	UserID    uuid.UUID
 	Items     []CartItem
 	CreatedAt time.Time
-	UpdateAt  time.Time
+	UpdatedAt time.Time
 }
 
 func NewCart(userID uuid.UUID) *Cart {
@@ -20,6 +21,155 @@ func NewCart(userID uuid.UUID) *Cart {
 		UserID:    userID,
 		Items:     []CartItem{},
 		CreatedAt: time.Now(),
-		UpdateAt:  time.Now(),
+		UpdatedAt: time.Now(),
 	}
+}
+
+func (c *Cart) AddItem(item CartItem) error {
+	if err := c.validateMaxLimitOfItems(); err != nil {
+		return err
+	}
+
+	for i, existingItem := range c.Items {
+		if existingItem.ProductID == item.ProductID {
+			// Actualizar cantidad en lugar de duplicar
+			c.Items[i].Quantity += item.Quantity
+			c.updateAction()
+			return nil
+		}
+	}
+
+	c.Items = append(c.Items, item)
+	c.updateAction()
+
+	return nil
+}
+
+func (c *Cart) RemoveItem(itemID uuid.UUID) error {
+	if err := c.validateNotEmptyCart(); err != nil {
+		return err
+	}
+
+	itemSeen := false
+	newItems := []CartItem{}
+
+	for _, item := range c.Items {
+		if item.ID != itemID {
+			newItems = append(newItems, item)
+		} else {
+			itemSeen = true
+		}
+	}
+
+	if !itemSeen {
+		return errors.New("item not found")
+	}
+
+	c.Items = newItems
+	c.updateAction()
+
+	return nil
+}
+
+func (c *Cart) Buy(excludeItemsIDs []*uuid.UUID) (float64, error) {
+	if err := c.validateNotEmptyCart(); err != nil {
+		return 0, err
+	}
+
+	if len(excludeItemsIDs) == 0 {
+		return c.completePurchase()
+	}
+
+	filteredItems := c.filterItems(excludeItemsIDs)
+	originalItems := c.Items
+
+	c.Items = filteredItems
+	subTotal, err := c.calculateTotal()
+	if err != nil {
+		c.Items = originalItems
+		return 0, err
+	}
+
+	c.completeCartAction()
+	return subTotal, nil
+}
+
+func (c *Cart) GetItemCount() int {
+	return len(c.Items)
+}
+
+func (c *Cart) GetItems() []CartItem {
+	return c.Items
+}
+
+func (c *Cart) CalculateTotal() (float64, error) {
+	return c.calculateTotal()
+}
+
+func (c *Cart) validateMaxLimitOfItems() error {
+	if len(c.Items) >= 20 {
+		return errors.New("cart: cannot add more than 20 items")
+	}
+	return nil
+}
+
+func (c *Cart) validateNotEmptyCart() error {
+	if len(c.Items) <= 0 {
+		return errors.New("cart: cart is empty")
+	}
+	return nil
+}
+
+func (c *Cart) clearCart() {
+	c.Items = []CartItem{}
+}
+
+func (c *Cart) completePurchase() (float64, error) {
+	subTotal, err := c.calculateTotal()
+	if err != nil {
+		return 0, err
+	}
+	c.completeCartAction()
+	return subTotal, nil
+}
+
+func (c *Cart) filterItems(excludeItemsIDs []*uuid.UUID) []CartItem {
+	excludeMap := c.createExcludeMap(excludeItemsIDs)
+	filteredItems := make([]CartItem, 0, len(c.Items))
+	for _, item := range c.Items {
+		if _, found := excludeMap[item.ID]; !found {
+			filteredItems = append(filteredItems, item)
+		}
+	}
+	return filteredItems
+}
+
+func (c *Cart) createExcludeMap(excludeItemsIDs []*uuid.UUID) map[uuid.UUID]bool {
+	excludeMap := make(map[uuid.UUID]bool)
+	for _, id := range excludeItemsIDs {
+		excludeMap[*id] = true
+	}
+	return excludeMap
+}
+
+func (c *Cart) completeCartAction() {
+	c.clearCart()
+	c.updateAction()
+}
+
+func (c *Cart) calculateTotal() (float64, error) {
+	if err := c.validateNotEmptyCart(); err != nil {
+		return 0, err
+	}
+
+	total := 0.0
+	for _, item := range c.Items {
+		total += (item.UnitPrice * float64(item.Quantity)) - item.Discount
+	}
+
+	return total, nil
+}
+
+func (c *Cart) updateAction() {
+	c.UpdatedAt = time.Now()
 }
--- services/cart_service/internal/core/domain/cart_item.go
+++ services/cart_service/internal/core/domain/cart_item.go
@@ -8,9 +8,22 @@ import (
 
 type CartItem struct {
 	ID        uuid.UUID
-	CartID    uuid.UUID
 	ProductID uuid.UUID
-	Quantity  uint
-	AddedAt   time.Time
-	UpdateAt  time.Time
+	Name      string
+	UnitPrice float64
+	Quantity  int
+	Discount  float64
+	CreatedAt time.Time
+}
+
+func NewCartItem(productID uuid.UUID, name string, unitPrice float64, quantity int, discount float64) CartItem {
+	return CartItem{
+		ID:        uuid.New(),
+		ProductID: productID,
+		Name:      name,
+		UnitPrice: unitPrice,
+		Quantity:  quantity,
+		Discount:  discount,
+		CreatedAt: time.Now(),
+	}
 }
